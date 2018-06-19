9f549aff453eaa05299344270e2e1f8fc692355b
winnerchou <33021992+winnerchou@users.noreply.github.com>
2018-06-19 20:45:06 +0800

    Add files via upload

---
+++ homework1-1.py
@@ -0,0 +1,11 @@
+#第一次課程作業
+n1=int(input("enter a number:"))
+n2=int(input("enter a number:"))
+n3=int(input("enter a number:"))
+n4=int(input("enter a number:"))
+n5=int(input("enter a number:"))
+List=[n1,n2,n3,n4,n5]
+X=sum(List)
+Y=max(List)
+print ("總和=",X)
+print ("最大值=",Y)
